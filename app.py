a082fab883f4e3ae3b4fee6f776f81e098253d74
m0nd2y <ldj6192@gmail.com>
2023-05-29 16:36:28 +0000

    convert return random -> modedl

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ get_next_action_ns = api.namespace('get_next_action', description='다음 action
 def get_next_action_func(file_content):
     state_list = list(file_content)
     model_name = 'DQNmodel_CNN'
-    test_model.test_main(state_list, model_name)
-    return random.randrange(0,7)
+    return test_model.test_main(state_list, model_name)
 
 Modelstate = get_next_action_ns.model('get_next_action', strict=True, model={
     'list': fields.List(fields.List(fields.Integer), title='현재 state', default=[[0,0,0,0,0,0,0], [0,0,0,0,0,2,0], [0,1,0,0,2,1,0], [0,2,0,0,1,1,0], [1,2,2,2,1,1,2], [1,2,2,1,1,2,1]], required=True),
